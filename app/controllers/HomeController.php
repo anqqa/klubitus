41417f9c7f0da999a30ccf287264151f4349fc6b
anqqa <anttiq@gmail.com>
2014-05-16 16:18:38 +0300

    Index view testing

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,7 +8,25 @@ class HomeController extends BaseController {
 	 * @return  \Illuminate\View\View
 	 */
 	public function getIndex() {
-		$this->view('home.index');
+		$this->layout->content = View::make('layouts._three_columns', array(
+			'left' => $this->_viewShouts()
+		));
+	}
+
+
+	protected function _viewBirthdays() {
+
+	}
+
+	protected function _viewNewsfeed() {
+
+	}
+
+
+	protected function _viewShouts() {
+		return View::make('home._shouts', array(
+			'title' => 'Shoutbox'
+		));
 	}
 
 }
