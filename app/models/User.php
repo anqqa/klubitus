3f982df94128b5744be715095490e60344ac93ef
anqqa <anttiq@gmail.com>
2014-06-02 15:02:15 +0300

    Light array

--- app/models/User.php
+++ app/models/User.php
@@ -14,6 +14,51 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 
 
 	/**
+	 * Load one user light array
+	 *
+	 * @static
+	 * @param   mixed  $id  User model, user array, user id, username
+	 * @return  array|null
+	 */
+	public static function findLight($id = null) {
+		if ($id instanceof User) {
+
+			// Got user model, no need to load, just fill caches
+			/** @var  User  $id */
+			return $id->toLightArray();
+
+		} else if (is_array($id)) {
+
+			// Got user array, don't fill caches as we're not 100% sure it's valid
+			return $id;
+
+		} else if (is_int($id) || is_numeric($id)) {
+
+			// Got an id
+			$id = (int)$id;
+
+		} else if (is_string($id)) {
+
+			// Got user name, find id
+			$id = self::user_id($id);
+
+		} else {
+
+			return null;
+
+		}
+
+		if ($id === 0) {
+			return null;
+		}
+
+		return Cache::remember('userLight:' . $id, 60 * 24, function() use ($id) {
+			return User::find($id)->toLightArray();
+		});
+	}
+
+
+	/**
 	 * Get the unique identifier for the user.
 	 *
 	 * @return mixed
@@ -30,6 +75,47 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	public function getAuthPassword() {
 		return $this->password;
+	}
+
+
+	/**
+	 * Get user id from data
+	 *
+	 * @static
+	 * @param   mixed  $user
+	 * @return  integer
+	 */
+	public static function getId($user) {
+		if (is_int($user) || is_numeric($user)) {
+
+			// Already got id
+			return (int)$user;
+
+		} else if (is_array($user)) {
+
+			// Got user array
+			return (int)Arr::get($user, 'id');
+
+		}	else if ($user instanceof User) {
+
+			// Got user model
+			return $user->id;
+
+		} else if (is_string($user)) {
+
+			// Got user name
+			$username = Text::slug($user);
+			if (!$id = (int)Cache::get('userName:' . $username)) {
+				if ($user = User::find_user($user)) {
+					$id = $user->id;
+					Cache::put('userName:' . $username, $id, 60 * 24);
+				}
+			}
+
+			return $id;
+		}
+
+		return 0;
 	}
 
 
@@ -83,6 +169,30 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 */
 	public function setRememberToken($value) {
 		$this->remember_token = $value;
+	}
+
+
+	/**
+	 * Convert to light array.
+	 *
+	 * @return  array
+	 */
+	public function toLightArray() {
+		if ($this->id) {
+			return array(
+				'id'           => (int)$this->id,
+				'username'     => $this->username,
+				'display_name' => $this->display_name,
+				'gender'       => $this->gender,
+				'title'        => $this->title,
+				'signature'    => $this->signature,
+				'avatar'       => $this->avatar,
+//				'thumb'        => $this->get_image_url('thumbnail'),
+				'last_login'   => (int)$this->last_login,
+			);
+		}
+
+		return array();
 	}
 
 
