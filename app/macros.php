2b6cc3d770cf3a6a8011a607cd182f0c6f0b76ef
anqqa <anttiq@gmail.com>
2014-06-02 15:01:10 +0300

    Form helpers

--- app/macros.php
+++ app/macros.php
@@ -1,7 +1,78 @@
 <?php
 
 /**
- * Print username.
+ * Form input.
+ */
+Form::macro('field', function($options) {
+
+	// Name, required
+	$name = array_pull($options, 'name');
+	if (!$name) {
+		return '';
+	}
+
+	// Type
+	$type = array_pull($options, 'type', 'text');
+
+	// Label
+	$label = array_pull($options, 'label');
+
+	// Value
+	$value = Input::old($name, array_pull($options, 'value'));
+
+	// Class
+	$class = array_pull($options, 'class');
+
+	// Error
+	$form  = array_pull($options, 'form');
+	$error = $form ? $form->getError($name) : null;;
+
+	// Add other parameters to input
+	$parameters = $options;
+	$parameters['class'] = trim('form-control ' . $class);
+
+	// Build markup
+	$markup = '<div class="form-group' . ($error ? ' has-error' : '') . '">';
+
+	switch ($type) {
+
+		case 'text':
+			$markup .= Form::label($name, $label, array('class' => 'control-label'));
+			$markup .= Form::input('text', $name, $value, $parameters);
+			break;
+
+		case 'checkbox':
+			$markup .= '<div class="checkbox"><label>';
+			$markup .= Form::checkbox($name, 1, (bool)$value);
+			$markup .= ' ' . $label;
+			$markup .= '</label></div>';
+			break;
+
+		case 'hidden':
+			$markup = Form::hidden($name, $value);
+			break;
+
+		case 'password':
+			$markup .= Form::label($name, $label, array('class' => 'control-label'));
+			$markup .= Form::password($name, $parameters);
+			break;
+
+	}
+
+	if ($error) {
+		$markup .= '<span class="help-block">' . $error . '</span>';
+	}
+
+	if ($type !== 'hidden') {
+		$markup .= '</div>';
+	}
+
+	return $markup;
+});
+
+
+/**
+ * Username with link to profile.
  */
 HTML::macro('user', function($user = null, $username = null, $url = null) {
 	static $viewer;
@@ -24,12 +95,13 @@ HTML::macro('user', function($user = null, $username = null, $url = null) {
 			$gender   = $user->gender;
 			$userId   = $user->id;
 			$username = $user->username;
-		} else if (is_numeric($user)) {
-			$userId = (int)$user;
-			if ($user = User::find($userId)) {
-				$gender   = $user->gender;
-				$username = $user->username;
+		} else if (is_numeric($user) || is_string($user)) {
+			if ($user = User::findLight($user)) {
+				$userId   = $user['id'];
+				$gender   = $user['gender'];
+				$username = $user['username'];
 			} else {
+				$userId   = 0;
 				$gender   = '';
 				$username = 'Unknown';
 			}
--- app/start/global.php
+++ app/start/global.php
@@ -19,6 +19,7 @@ ClassLoader::addDirectories(array(
 	app_path() . '/database/seeds',
 	app_path() . '/exceptions',
 	app_path() . '/libraries',
+	app_path() . '/forms',
 
 ));
 
